75d5cbe1a9f197d0568f6be3206c63b9c6473e1e
Unknown <wueric0622@gmail.com>
2018-07-14 14:21:14 +0800

    更新場景生成系統

--- Assets/Scripts/SceneGeneratorSystem.cs
+++ Assets/Scripts/SceneGeneratorSystem.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class SceneGeneratorSystem : MonoBehaviour {
 
+    static public SceneGeneratorSystem instance;
 
+    [SerializeField] private Transform[] spawnPoint_Up;
+    [SerializeField] private Transform[] spawnPoint_Down;
+    [SerializeField] private GameObject[] item;
 
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void StartGenerat()
+    {
+        StartCoroutine(Generating());
+    }
+
+    IEnumerator Generating()
+    {
+        yield return 0;
+    }
 }
