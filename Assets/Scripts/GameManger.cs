24d466ad197062407ab7f9423e707ed0b5a3136f
Unknown <wueric0622@gmail.com>
2018-07-14 12:15:31 +0800

    AddGameManger

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+
+    static public GameManger instance;
+
+	void Awake()
+	{
+		if(instance == null)
+		{
+            instance = this;
+        }
+		else
+		{
+            Destroy(this);
+        }
+	}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
